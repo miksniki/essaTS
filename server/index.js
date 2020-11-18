31fca090165aab18eda788cab39b2a9b1ae5b30a
hypeScript <nikita.mikson@gmail.com>
2020-11-18 20:28:53 +0200

    download ja concat songs done

--- server/index.js
+++ server/index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const path = require('path');
-// const ffmpeg = require('fluent-ffmpeg');
-const fileUpload = require('express-fileupload')
+const ffmpeg = require('fluent-ffmpeg');
+const probe = require('ffmpeg-probe');
+const audioConcat = require('audioconcat');
+const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const fs = require('fs');
 
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, 'edited')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -38,7 +47,7 @@ app.post('/upload', async (req, res) => {
                 const {file} = req.files
 
                 file.mv('uploads/' + file.name) 
-
+               
                 res.send({
                     message: 'file is uploaded'
                 })
@@ -52,7 +61,8 @@ const uploads = './uploads';
 songs = [];
 function readDirectory(callback) {
     fs.readdir(uploads, (err, files) => {
-        songs.push(files);
+        const originalFiles = files.filter(fileName => !songs.includes(fileName))
+        songs = [...songs, ...(originalFiles||[])]
         callback(songs);
     });
 }
@@ -63,16 +73,69 @@ app.get('/songs', (req, res) => {
     });
 });
 
- /* app.post('/upload/:filename/:action', async (req, res) => {
-    const filename = req.params.filename
-    if (req.params.action === 'split') {       
-        res.send({
-            message: 'No files uploaded'
-        }); */ 
+const edits = './edited';
+editSongs = [];
+function readEditDirectory(callback) {
+    fs.readdir(edits, (err, files) => {
+    const originalFiles = files.filter(fileName => !editSongs.includes(fileName))
+    editSongs = [...editSongs, ...(originalFiles||[])]
+    callback(editSongs);
+    });
+}
+
+app.get('/editedSongs', (req, res) => {
+    readEditDirectory(function(editSongFiles) {
+        res.send({files: editSongFiles})
+    });
+});
+
 
 // FFMPEG configuration
 
 
-//
+app.get('/:filename/:action', async (req, res) => {
+    if (req.params.action === 'split') {   
+        const fileName = req.params.filename
+        const fileLocation = `${__dirname}/uploads/${fileName}`
+        const info = await probe(fileLocation)
+        const duration = info.duration;
+        const realDuration = duration * 0.001
+        const clipDuration = parseInt(realDuration / 2);
+
+        ffmpeg(fileLocation)
+        .inputOption([ `-ss 0`])
+        .outputOptions([ `-t ${clipDuration}`])
+        .output(`./edited/${fileName}split.mp3`)
+        .on('end', () => console.log('Split successful!'))
+        .on('error', (err) => console.log(err))         
+        .run()
+        
+    };
+});
+
+app.post('/concat', async (req, res) => {     
+    const fileArray = req.body
+    const fileNames = (req.body[0] + '+' + req.body[1])
+    const songsPath = fileArray.map((file) => { 
+        let pathsArray = [];
+        pathsArray.push(`${__dirname}/uploads/${file}`);
+        return pathsArray;
+    })
+    const mergedArray = [].concat.apply([], songsPath);
+    audioConcat(mergedArray)
+    .concat(`./edited/concat${fileNames}.mp3`)
+    .on('start', function(command) {
+        console.log('ffmpeg process started:', command)
+        res.send({
+            message: 'file is uploaded'
+        })
+    })
+    .on('error', function(err, stdout, stderr) {
+        console.error('Error:', err)
+        console.error('ffmpeg stderr:', stderr)
+    });
+});
+
+
 
 app.listen(5000);
